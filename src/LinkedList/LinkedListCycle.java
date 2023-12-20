38b9f0fe3cea78c06b7b206ba76d4faa2396d35b
konstantin13-hi <kostya.chizhevskiy@mail.ru>
2023-12-21 00:48:28 +0100

    listcycle

---
+++ src/LinkedList/LinkedListCycle.java
@@ -0,0 +1,69 @@
+package LinkedList;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LinkedListCycle {
+
+
+
+
+
+       static class  ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) {
+              val = x;
+              next = null;
+          }
+      }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode node =head;
+        ListNode nodeSecond = head;
+
+        while (node !=null){
+            if(nodeSecond!=null) {
+                nodeSecond = nodeSecond.next;
+            }
+            if(nodeSecond!=null){
+                nodeSecond= nodeSecond.next;
+            }
+            node= node.next;
+            if(node == nodeSecond && node!=null){
+                return true;
+            }
+
+        }
+
+      return false;
+    }
+
+    public static boolean hasCycle2(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+      //  head.next.next.next.next = head.next;
+        boolean hasCycle = hasCycle(head);
+        System.out.println("Output: " + hasCycle);
+
+    }
+}
